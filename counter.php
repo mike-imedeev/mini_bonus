e991da3ad7ae2997e86db293273dc1f40e021d70
mike-imedeev <mike.imedeev@gmail.com>
2016-09-19 00:14:43 +0800

    111
    1

---
+++ counter.php
@@ -0,0 +1,21 @@
+<?php
+
+$f=fopen("stat.dat","a+");
+flock($f,LOCK_EX);
+$count=fread($f,100);
+@$count++;
+ftruncate($f,0);
+fwrite($f,$count);
+fflush($f);
+flock($f,LOCK_UN);
+fclose($f);
+
+$f=fopen("stat_ip.dat","a+");
+fwrite($f,date("Y.m.d h:I:s").'	'.$count.'	'.$_SERVER['REMOTE_ADDR'].'	'.$_SERVER['HTTP_USER_AGENT']."\r\n");
+fclose($f);
+
+
+echo "Количество просмотров: $count"; 
+echo " mike - 2016" ;
+#test
+?>
