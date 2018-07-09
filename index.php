0d1756b73ce6f2cf383d686de38325af5a6c5d35
mike-imedeev <mike.imedeev@gmail.com>
2018-07-09 15:12:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,6 @@ print '<html><head>';
 print '<title>???</title>';
 print '<script type="text/javascript" src="//vk.com/js/api/openapi.js?129"></script>';
 print '</head><body>';
-print 'Женя дурак!<hr>';
-print '<script type="text/javascript">VK.init({apiId: 5631629, onlyWidgets: true});</script><div id="vk_comments"></div><script type="text/javascript">VK.Widgets.Comments("vk_comments", {limit: 20, width: "665", attach: "*"});</script>';
 print '<hr>';
 include 'counter.php';
 print '</body></html>'; 
